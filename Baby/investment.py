67624a498a1c94d65b833a3a658f49a2910730b5
chenzuoyuan <2536566061@qq.com>
2018-05-10 15:47:20 +0800

    Python3

---
+++ Baby/investment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+amount = float(input("Enter amout:"))  #输入数额
+inrate = float(input("Enter interest rate"))
+period = int(input("Enter period"))
+value = 0
+year = 1
+while year <= period:
+    value = amount + (inrate * amount)
+    print("Year {} Rs. {:.2f}".format(year,value))
+    amount = value
+    year = year+1
+
